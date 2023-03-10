7fc7e64c19ba5194104f441ba74196cd8053c807
uday <121241786+uday1508@users.noreply.github.com>
2023-03-10 18:49:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,16 @@
 
+import Home from './Home.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
+
+
+import React, { Component } from 'react'
